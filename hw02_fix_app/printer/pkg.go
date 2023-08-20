d81658cd2ca69ec576a22a74d35d0f65eb9e69b9
Sergey Yarin <79122811469@yandex.ru>
2023-08-21 00:38:03 +0500

    fix formatting

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -10,7 +10,7 @@ func PrintStaff(staff []types.Employee) {
 	var str string
 	for i := 0; i < len(staff); i++ {
 		str := fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ",
-				   staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
+			staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
 		fmt.Println(str)
 	}
 
