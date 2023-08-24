8ccfc78778decb8d3d68cacdce4a7c410cf1660f
Sergey Yarin <79122811469@yandex.ru>
2023-08-24 21:31:24 +0500

    simplify printer using type's String method

--- hw02_fix_app/printer/pkg.go
+++ hw02_fix_app/printer/pkg.go
@@ -8,8 +8,6 @@ import (
 
 func PrintStaff(staff []types.Employee) {
 	for i := 0; i < len(staff); i++ {
-		str := fmt.Sprintf("User ID: %d; Age: %d; Name: %s; Department ID: %d; ",
-			staff[i].UserID, staff[i].Age, staff[i].Name, staff[i].DepartmentID)
-		fmt.Println(str)
+		fmt.Println(staff[i])
 	}
 }
