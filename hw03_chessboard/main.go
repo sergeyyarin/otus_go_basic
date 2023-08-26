e4f842621914c5ac9a5be87f860c07317465d27b
Alena Yarina <2622428@mail.ru>
2023-08-26 13:10:11 +0500

    initial

--- hw03_chessboard/main.go
+++ hw03_chessboard/main.go
@@ -1,5 +1,42 @@
 package main
 
+import (
+	"errors"
+	"fmt"
+)
+
+func buildRow(length int) (row string, err error) {
+	if valid := length > 0; !valid {
+		err = errors.New("row len is not valid")
+		return row, err
+	}
+	if even := length%2 == 0; !even {
+		err = errors.New("row len is not even")
+		return row, err
+	}
+
+	const (
+		space = " "
+		hash  = "#"
+	)
+
+	for i := 0; i <= length; i++ {
+		if i%2 == 0 {
+			row += hash
+		} else {
+			row += space
+		}
+	}
+
+	return row, err
+}
+
 func main() {
-	// Place your code here.
+	length := 8
+	row, err := buildRow(length)
+	if err != nil {
+		fmt.Println("Error: ", err)
+	} else {
+		fmt.Println("Board: ", row)
+	}
 }
