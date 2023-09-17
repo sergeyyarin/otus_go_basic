c40dcd7f89a258b084c80e5e65a4b38a3be1d7d3
Sergey Yarin <79122811469@yandex.ru>
2023-09-17 06:32:40 +0500

    add setters and getters

--- hw04_struct_comparator/book/pkg.go
+++ hw04_struct_comparator/book/pkg.go
@@ -1,7 +1,8 @@
 package book
 
+var id int
+
 func newID() func() int {
-	var id int
 	return func() int {
 		id++
 		return id
@@ -17,13 +18,52 @@ type Book struct {
 	rate   float64
 }
 
-func NewBook(title, author string, year, size int, rate float64) Book {
+func NewBook() Book {
 	return Book{
-		id:     newID()(),
-		title:  title,
-		author: author,
-		year:   year,
-		size:   size,
-		rate:   rate,
+		id: newID()(),
 	}
+}
+
+func (r Book) GetID() int {
+	return r.id
+}
+
+func (r Book) GetTitle() string {
+	return r.title
+}
+
+func (r Book) GetAuthor() string {
+	return r.author
+}
+
+func (r Book) GetYear() int {
+	return r.year
+}
+
+func (r Book) GetSize() int {
+	return r.size
+}
+
+func (r Book) GetRate() float64 {
+	return r.rate
+}
+
+func (r *Book) SetTitle(new string) {
+	r.title = new
+}
+
+func (r *Book) SetAuthor(new string) {
+	r.author = new
+}
+
+func (r *Book) SetYear(new int) {
+	r.year = new
+}
+
+func (r *Book) SetSize(new int) {
+	r.size = new
+}
+
+func (r *Book) SetRate(new float64) {
+	r.rate = new
 }
--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -7,6 +7,20 @@ import (
 )
 
 func main() {
-	b1 := book.NewBook("Title1", "Author1", 1928, 500, 4.6)
-	fmt.Printf("%v", b1)
+	b1 := book.NewBook()
+	b1.SetAuthor("Author1")
+	b1.SetTitle("Title1")
+	b1.SetYear(1928)
+	b1.SetSize(500)
+	b1.SetRate(4.6)
+
+	b2 := book.NewBook()
+	b2.SetAuthor("Author2")
+	b2.SetTitle("Title2")
+	b2.SetYear(1828)
+	b2.SetSize(300)
+	b2.SetRate(2.6)
+
+	fmt.Printf("%v\n", b1)
+	fmt.Printf("%v\n", b2)
 }
