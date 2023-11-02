be0af1afce18253ec382b44f16f43751127b9122
Sergey Yarin <yarin@iskrauraltel.ru>
2023-11-02 12:40:33 +0500

    implement comparison

--- hw04_struct_comparator/book/pkg.go
+++ hw04_struct_comparator/book/pkg.go
@@ -1,13 +1,5 @@
 package book
 
-func newID() func() int {
-	var id int
-	return func() int {
-		id++
-		return id
-	}
-}
-
 type Book struct {
 	id     int
 	title  string
@@ -17,13 +9,88 @@ type Book struct {
 	rate   float64
 }
 
-func NewBook(title, author string, year, size int, rate float64) Book {
-	return Book{
-		id:     newID()(),
-		title:  title,
-		author: author,
-		year:   year,
-		size:   size,
-		rate:   rate,
+func (b *Book) SetID(newID int) {
+	b.id = newID
+}
+
+func (b *Book) SetTitle(newTitle string) {
+	b.title = newTitle
+}
+
+func (b *Book) SetAuthor(newAuthor string) {
+	b.author = newAuthor
+}
+
+func (b *Book) SetYear(newYear int) {
+	b.year = newYear
+}
+
+func (b *Book) SetSize(newSize int) {
+	b.size = newSize
+}
+
+func (b *Book) SetRate(newRate float64) {
+	b.rate = newRate
+}
+
+func (b *Book) GetID() int {
+	return b.id
+}
+
+func (b *Book) GetTitle() string {
+	return b.title
+}
+
+func (b *Book) GetAuthor() string {
+	return b.author
+}
+
+func (b *Book) GetYear() int {
+	return b.year
+}
+
+func (b *Book) GetSize() int {
+	return b.size
+}
+
+func (b *Book) GetRate() float64 {
+	return b.rate
+}
+
+type CompareMode int
+
+const (
+	Year CompareMode = iota
+	Size
+	Rate
+)
+
+type BookComparator struct {
+	Mode CompareMode
+	Lhs  *Book
+	Rhs  *Book
+}
+
+func (c *BookComparator) Compare() *Book {
+	switch c.Mode {
+	case Year:
+		if c.Lhs.year > c.Rhs.year {
+			return c.Lhs
+		} else {
+			return c.Rhs
+		}
+	case Size:
+		if c.Lhs.size > c.Rhs.size {
+			return c.Lhs
+		} else {
+			return c.Rhs
+		}
+	case Rate:
+		if c.Lhs.rate > c.Rhs.rate {
+			return c.Lhs
+		} else {
+			return c.Rhs
+		}
 	}
+	return nil
 }
--- hw04_struct_comparator/main.go
+++ hw04_struct_comparator/main.go
@@ -6,7 +6,44 @@ import (
 	"github.com/sergeyyarin/otus_go_basic/hw04_struct_comparator/book"
 )
 
+func compareBooks(lhs *book.Book, rhs *book.Book, mode book.CompareMode) *book.Book {
+	compare := book.BookComparator{Mode: mode, Lhs: lhs, Rhs: rhs}
+	if res := compare.Compare(); res != nil {
+		return res
+	}
+	return nil
+}
+
 func main() {
-	b1 := book.NewBook("Title1", "Author1", 1928, 500, 4.6)
-	fmt.Printf("%v", b1)
+	a := book.Book{}
+	b := book.Book{}
+
+	a.SetID(1)
+	a.SetTitle("Title1")
+	a.SetAuthor("Author1")
+	a.SetYear(1901)
+	a.SetSize(100)
+	a.SetRate(8.7)
+
+	b.SetID(2)
+	b.SetTitle("Title2")
+	b.SetAuthor("Author2")
+	b.SetYear(2009)
+	b.SetSize(1450)
+	b.SetRate(4.8)
+
+	bigger := compareBooks(&a, &b, book.Size)
+	if bigger != nil {
+		fmt.Printf("%s by %s is bigger\n", bigger.GetTitle(), bigger.GetAuthor())
+	}
+
+	newer := compareBooks(&a, &b, book.Year)
+	if newer != nil {
+		fmt.Printf("%s by %s is more recent\n", newer.GetTitle(), newer.GetAuthor())
+	}
+
+	best := compareBooks(&a, &b, book.Rate)
+	if best != nil {
+		fmt.Printf("%s by %s is more popular\n", best.GetTitle(), best.GetAuthor())
+	}
 }
