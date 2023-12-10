098d03a0b35b8112bc0cefb2ecf45ed1692f8e4c
Sergey Yarin <79122811469@yandex.ru>
2023-12-10 18:40:33 +0500

    fix range loop error

--- hw05_shapes/cmd/main.go
+++ hw05_shapes/cmd/main.go
@@ -2,18 +2,20 @@ package main
 
 import (
 	"fmt"
+
+	"github.com/sergeyyarin/otus_go_basic/tree/hw05_shapes/hw05_shapes/internal/shape"
 )
 
 func calculateArea(s any) (float32, error) {
-	if v, ok := s.(Circle); ok {
+	if v, ok := s.(shape.Circle); ok {
 		fmt.Printf("Circe: radius %f\n", v.Radius())
 		return v.Area(), nil
 	}
-	if v, ok := s.(Rectangle); ok {
+	if v, ok := s.(shape.Rectangle); ok {
 		fmt.Printf("Rectangle: length %f, width %f\n", v.Length(), v.Width())
 		return v.Area(), nil
 	}
-	if v, ok := s.(Triangle); ok {
+	if v, ok := s.(shape.Triangle); ok {
 		fmt.Printf("Triangle: base %f, height %f\n", v.Base(), v.Height())
 		return v.Area(), nil
 	}
@@ -23,22 +25,22 @@ func calculateArea(s any) (float32, error) {
 func main() {
 	var shapes []any
 
-	var c Circle
+	var c shape.Circle
 	c.SetRadius(5)
 
-	var r Rectangle
+	var r shape.Rectangle
 	r.SetLength(10)
 	r.SetWidth(5)
 
-	var t Triangle
+	var t shape.Triangle
 	t.SetBase(8)
 	t.SetHeight(6)
 
 	var i int
 
-	shapes = append(shapes, i, c, r, t)
+	shapes = append(shapes, c, r, t, i)
 
-	for shape := range shapes {
+	for _, shape := range shapes {
 		if area, err := calculateArea(shape); err != nil {
 			fmt.Println(err)
 		} else {
