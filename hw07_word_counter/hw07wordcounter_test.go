9990848cfbbea7f623b26263cf7b871239f394f8
Sergey Yarin <79122811469@yandex.ru>
2023-12-21 21:22:12 +0500

    add test

---
+++ hw07_word_counter/hw07wordcounter_test.go
@@ -0,0 +1,44 @@
+package wordcounter_test
+
+import (
+	"reflect"
+	"testing"
+
+	"github.com/sergeyyarin/otus_go_basic/hw07_word_counter/wordcounter"
+)
+
+func TestCountWords(t *testing.T) {
+	testCases := map[string]struct {
+		input string
+		want  map[string]int
+	}{
+		"empty input": {
+			input: "",
+			want:  map[string]int{},
+		},
+		"input with spaces": {
+			input: "  foo bar  baz   foo",
+			want: map[string]int{
+				"foo": 2,
+				"bar": 1,
+				"baz": 1,
+			},
+		},
+		"input with mixed spaces and punctuation": {
+			input: "  foo;bar,baz..foo.       ,bar;baz",
+			want: map[string]int{
+				"foo": 2,
+				"bar": 2,
+				"baz": 2,
+			},
+		},
+	}
+	for name, tc := range testCases {
+		t.Run(name, func(t *testing.T) {
+			got := wordcounter.CountWords(tc.input)
+			if !reflect.DeepEqual(tc.want, got) {
+				t.Errorf("not equal: got %v; want %v", got, tc.want)
+			}
+		})
+	}
+}
